4cf391fd971fad7d90e05402d0697107567e6617
Gabriel Romero <gabester78@gmail.com>
2020-05-21 09:29:02 -0500

    Cleaned up code.

--- src/Components/cardStyling.js
+++ src/Components/cardStyling.js
@@ -45,8 +45,6 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-// export default function SimpleTabs() {
-
 const CardStyling = (props) => {
   const classes = useStyles();
   const [value, setValue] = React.useState(0);
@@ -56,8 +54,9 @@ const CardStyling = (props) => {
   };
 
   return (
-    <div className="d-flex justify-content-center">
-      <Card className="border border-light p-3 w-50 bg-secondary ">
+    <div className="d-flex justify-content-center mt-5 mb-5">
+      <Card className="border border-light p-3 w-75 bg-secondary ">
+        <h1 className="text-light text-uppercase">Nasa photo of the day</h1>
         <CardImg
           top
           width="100%"
@@ -86,28 +85,11 @@ const CardStyling = (props) => {
           <h1 className="text-light mb-2">{props.data.date}</h1>
         </TabPanel>
         <CardBody className="p-0">
-          {/* <h1 className="text-light text-uppercase">{props.data.title}</h1> */}
-          {/* <h3 className="text-light mb-2">Author: {props.data.copyright}</h3> */}
-          {/* <h4 className="text-light mb-2">Date: {props.data.date}</h4> */}
           <p className="text-light mb-0">{props.data.explanation}</p>
         </CardBody>
       </Card>
     </div>
   );
 };
-// <div className="container">
-//   <header>
-//     <h1>{props.data.title}</h1>
-//     <h3>Author: {props.data.copyright}</h3>
-//     <h4>{props.data.date}</h4>
-//   </header>
-//   <div className="imgContainer">
-//     <img className="iotd" alt="Image of the day" src={props.data.url}></img>
-//     <h3>About this photo</h3>
-//     <p>{props.data.explanation}</p>
-//   </div>
-// </div>
-//   );
-// };
 
 export default CardStyling;
